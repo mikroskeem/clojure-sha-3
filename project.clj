0707d906d6bd8654fd31c434d43bca2702ea4568
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2020-02-15 04:09:26 +0200

    Add deployment configuration

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject clojure-sha-3 "0.1.0"
   :description ""
   :url ""
+  :deploy-repositories [["releases"  {:url "https://repo.wut.ee/repository/mikroskeem-repo" :creds :gpg}]
+                        ["snapshots" {:url "https://repo.wut.ee/repository/mikroskeem-repo" :creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [prismatic/hiphip "0.2.0"]]
   :jvm-opts ^:replace [])
